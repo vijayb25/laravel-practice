4632c45812f0ee7c397da58e204ca499aa9c85d8
Vijay Baclay <vijay.baclay@proweaver.net>
2024-06-03 13:29:28 +0800

    First push

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot name="heading">
+        About Page
+    </x-slot>
+    <h1>Hello from the About Page</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot name="heading">
+        Contact Page
+    </x-slot>
+    <h1>Hello from the Contact Page</h1>
+</x-layout>
